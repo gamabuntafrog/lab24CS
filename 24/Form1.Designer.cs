6778018030ecc371b03a9f06fbb97ab73f5fa710
Yaroslaw Kyrylenko <83594359+gamabuntafrog@users.noreply.github.com>
2023-05-23 20:54:20 +0300

    Add files via upload

---
+++ 24/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(662, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(662, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ 24/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp25
+{
+    public partial class Form1 : Form
+    {
+        private Button movingButton;
+        private Timer timer;
+        private Random random;
+        private int score;
+        private int secondsLeft;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+        private void InitializeGame()
+        {
+            movingButton = new Button();
+            movingButton.BackColor = Color.Red;
+            movingButton.Width = 50;
+            movingButton.Height = 50;
+            movingButton.Click += MovingButton_Click;
+            Controls.Add(movingButton);
+
+            timer = new Timer();
+            timer.Interval = 700;
+            timer.Tick += Timer_Tick;
+
+            random = new Random();
+            score = 0;
+            secondsLeft = 30;
+        }
+        private void StartGame()
+        {
+            int x = random.Next(0, ClientSize.Width - movingButton.Width);
+            int y = random.Next(0, ClientSize.Height - movingButton.Height);
+            movingButton.Location = new Point(x, y);
+            movingButton.Visible = true;
+            timer.Start();
+        }
+        private void StopGame()
+        {
+            timer.Stop();
+            movingButton.Visible = false;
+
+            if (secondsLeft == 0)
+            {
+                string message = $"Гра закінчена!\nScore: {score}\n\nХочете зберегти свої очки?";
+                DialogResult result = MessageBox.Show(message, "Гра закінчена", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    SaveScore();
+                }
+            }
+        }
+        private void MovingButton_Click(object sender, EventArgs e)
+        {
+            if (secondsLeft > 0)
+            {
+                int width = random.Next(20, 100);
+                int height = random.Next(20, 100);
+                movingButton.Size = new Size(width, height);
+                score++;
+                label1.Text = $"Score: {score}";
+            }
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            secondsLeft--;
+            label2.Text = $"Залишилось часу: {secondsLeft} секунд";
+
+            if (secondsLeft == 0)
+            {
+                StopGame();
+            }
+            else
+            {
+                int x = random.Next(0, ClientSize.Width - movingButton.Width);
+                int y = random.Next(0, ClientSize.Height - movingButton.Height);
+                movingButton.Location = new Point(x, y);
+            }
+        }
+        private void SaveScore()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files|*.txt";
+            saveFileDialog.Title = "Save Score";
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.FileName = "Scores.txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                using (System.IO.StreamWriter writer = new System.IO.StreamWriter(filePath))
+                {
+                    writer.WriteLine($"Score: {score}");
+                    writer.WriteLine($"Date: {DateTime.Now}");
+                }
+
+                MessageBox.Show("Очки були збережені.", "Save Score", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
